549378b46456aba6fe59d70cc957d247426ae659
Suyen Shrestha <suyencrestha@gmail.com>
2023-04-08 15:29:38 +0545

    feat: added consumer and routing for the chatroom.

--- core/settings.py
+++ core/settings.py
@@ -74,6 +74,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+    'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
