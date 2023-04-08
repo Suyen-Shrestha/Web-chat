19a34483f5b489f8bce164fc9c66ff342ba0cbf5
Suyen Shrestha <suyencrestha@gmail.com>
2023-04-08 14:06:34 +0545

    feat: implemented user login functionality.

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
 app_name = 'chat'
@@ -8,5 +8,6 @@ app_name = 'chat'
 urlpatterns = [
     path('', views.startpage, name="start-page"),
     path('signup/', views.signup, name="signup"),
-    path('login/', LogoutView.as_view(), name="logout"),
+    path('login/', LoginView.as_view(template_name='chat/login.html'), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
