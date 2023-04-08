b0a6d724e4ed5b653626bc58fd01203816fee4d5
Suyen Shrestha <suyencrestha@gmail.com>
2023-04-08 13:54:02 +0545

    feat: implemented user signup functionality.

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import login
+from .forms import SigupForm
 
 
 def startpage(request):
     return render(request, 'chat/startpage.html')
+
+
+
+def signup(request):
+    form = SigupForm()
+    if request.method == 'POST':
+        form = SigupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return reverse('chat:start-page')
+    return render(request, 'chat/signup.html', {'form': form})
